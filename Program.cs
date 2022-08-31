f67f422cca656698f689558cba9370648e8320f9
Rudolf Neyman <neyman.rudolf@yandex.ru>
2022-08-31 19:08:24 +0300

    Решение Задачи 10 в двух вариантах

--- Program.cs
+++ Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+// Вариант 1
+// string? number = Console.ReadLine();
+// Console.WriteLine( number [1]);
+
+// Вариант 2 
+// int number = new Random().Next(100,1000);
+// Console.WriteLine(number);
+// int firstNumber = number/10;
+// int secondNumber = firstNumber%10;
+// Console.WriteLine($"Вторая цифра в числе: {secondNumber}");
