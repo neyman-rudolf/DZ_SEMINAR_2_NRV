657871f25b87127403352dec7ff6ea5b00504545
Rudolf Neyman <neyman.rudolf@yandex.ru>
2022-09-01 16:00:27 +0300

    Решение Задачи 13

--- Program.cs
+++ Program.cs
@@ -14,3 +14,16 @@
 // int firstNumber = number/10;
 // int secondNumber = firstNumber%10;
 // Console.WriteLine($"Вторая цифра в числе: {secondNumber}");
+
+// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+string? number = Console.ReadLine();
+
+int size = number.Length;
+
+if (number.Length < 3 ) Console.WriteLine("Нет третьего числа");
+
+else Console.WriteLine(number [2]);
